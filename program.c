39f444c2b7aeda7abc919e4a9b2d9dcd59e88d99
yuvrajsinghalyuvrajsinghal86-boop <yuvrajsinghalyuvrajsinghal86@gmail.com>
2026-07-16 12:20:50 +0530

    Create program.c

---
+++ program.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    printf("\n\nt enter a number");
+    scanf("%d%d",&a,&b);
+            printf("\n\n\t %d + %d = %d",a,b,a+b);
+            printf("\n\n\t %d - %d = %d",a,b,a-b);
+            printf("\n\n\t %d * %d = %d",a,b,a*b);
+            printf("\n\n\t %d / %d = %d",a,b,a/b);
+    return 0;
+}
